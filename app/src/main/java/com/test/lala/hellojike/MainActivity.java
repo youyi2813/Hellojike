876113cd464db222beccbb6300d1f1a382fed1f6
YouYi <youyi2813@163.com>
2018-12-10 14:33:09 +0800

    测试git_push

--- app/src/main/java/com/test/lala/hellojike/MainActivity.java
+++ app/src/main/java/com/test/lala/hellojike/MainActivity.java
@@ -10,8 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //TODO 帮我在这里协助做点事
         for(int i = 0;i<2;i++)
-            System.out.println(i);
+            // 本地修改，尝试push
+            System.out.println(i+2);
         // 测试git push跟pull
+
         setContentView(R.layout.activity_main);
     }
 }
